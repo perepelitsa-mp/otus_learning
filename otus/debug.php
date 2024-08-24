62a8dbc70cb497094090877924fb4f66b8199233
Perepelitsa <i@perepelitsa-mp.ru>
2024-08-25 09:46:58 +1000

    ДЗ по логгеру

--- otus/debug.php
+++ otus/debug.php
@@ -2,9 +2,16 @@
 $logFile = $_SERVER['DOCUMENT_ROOT'] . '/logs/dateAndTime.log';
 $currentDateTime = date('Y-m-d H:i:s');
 if (!file_exists($logFile)) {
-    touch($logFile);
+    if (touch($logFile)) {
+        echo "Файл test.log успешно создан.";
+    } else {
+        echo "Не удалось создать файл test.log.";
+    }
 }
 if (is_writable($logFile)) {
     file_put_contents($logFile, "Текущее время и дата: " . $currentDateTime . PHP_EOL, FILE_APPEND);
+    echo "Дата и время успешно записаны в файл test.log.";
+} else {
+    echo "Файл test.log недоступен для записи.";
 }
 ?>
