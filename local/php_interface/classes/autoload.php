281962407cf7a71336d50c0916e07f5fb961ce30
Perepelitsa <i@perepelitsa-mp.ru>
2024-08-27 22:58:28 +1000

    ДЗ по логгеру

--- local/php_interface/classes/autoload.php
+++ local/php_interface/classes/autoload.php
@@ -4,20 +4,18 @@ if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) {
     die();
 }
 
-spl_autoload_register(function (string $class): void
+spl_autoload_register(function(string $class): void
 {
-    if (!str_contains($class, 'Otus')) {
+    if(!str_contains( $class, 'Otus')){
         return;
     }
 
-    var_dump($class);
-
     $class = str_replace('\\', '/', $class);
+
     $path = __DIR__ . '/' . $class . '.php';
 
-    var_dump($path);
 
-    if (is_file($path)) {
+    if(is_file($path)){
         require_once $path;
     }
 });
--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -1,8 +1,14 @@
 <?php
-define('DEBUG_FILE_NAME', $_SERVER["DOCUMENT_ROOT"] . '/logs/' . date("Y-m-d") . '.log');
+define('DEBUG_FILE_NAME', $_SERVER["DOCUMENT_ROOT"] .'/logs/OTUS_test.log');
 
 if (file_exists(__DIR__ . '/classes/autoload.php')) {
     require_once __DIR__ . '/classes/autoload.php';
 }
 
-\Otus\Diagnostic\myNewHelper::writeToLog('Тестовая запись в лог');
+//\Otus\Diagnostic\OtusNewHelper::writeToLog('Hello, world!', 'test');
+
+try {
+    throw new \Exception('Hello, world!'); // Создаем исключение с сообщением
+} catch (\Exception $e) {
+    \Otus\Diagnostic\OtusNewHelper::writeToLog($e, 'test');
+}
