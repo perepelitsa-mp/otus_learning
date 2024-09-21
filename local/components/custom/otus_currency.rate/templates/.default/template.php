62f6cddf0217779d91882a41f903ad3b081b8cc9
perepelitsa-mp <89619017+perepelitsa-mp@users.noreply.github.com>
2024-09-21 10:01:40 +1000

    ДЗ по Компонент списка таблицы

--- local/components/custom/otus_currency.rate/templates/.default/template.php
+++ local/components/custom/otus_currency.rate/templates/.default/template.php
@@ -1,15 +1,12 @@
 <?php if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die(); ?>
+
 <?php
 $templateFolder = $this->GetFolder();
 $APPLICATION->SetAdditionalCSS($templateFolder . "/styles.css");
 ?>
 
 <div class="currency-container">
-    <div class="currency-result" id="currency-result">
-        <?php if (!empty($arResult['SELECTED_CURRENCY'])): ?>
-            <p>Курс валюты <strong><?= htmlspecialcharsbx($arResult['SELECTED_CURRENCY']) ?></strong>: <?= htmlspecialcharsbx($arResult['SELECTED_RATE']) ?></p>
-        <?php endif; ?>
-    </div>
+    <h2>Курсы валют</h2>
     <form method="post" class="currency-form" id="currency-form">
         <?= bitrix_sessid_post() ?>
         <label for="currency-select">Выберите валюту:</label>
@@ -21,23 +18,30 @@ $APPLICATION->SetAdditionalCSS($templateFolder . "/styles.css");
             <?php endforeach; ?>
         </select>
     </form>
+
+    <div class="currency-result" id="currency-result">
+        <?php if (!empty($arResult['SELECTED_CURRENCY'])): ?>
+            <p>Курс валюты <strong><?= htmlspecialcharsbx($arResult['SELECTED_CURRENCY']) ?></strong>: <?= htmlspecialcharsbx($arResult['SELECTED_RATE']) ?></p>
+        <?php endif; ?>
+    </div>
 </div>
 
 <script>
     document.addEventListener('DOMContentLoaded', function() {
-        let currencies = <?= CUtil::PhpToJSObject($arResult['CURRENCIES']) ?>
-        let currencySelect = document.getElementById('currency-select')
-        let currencyResult = document.getElementById('currency-result')
-        function updateCurrencyRate() {
-            let selectedCurrency = currencySelect.value
-            let rate = currencies[selectedCurrency]['AMOUNT']
-            let currencyCode = selectedCurrency
+        var currencies = <?= CUtil::PhpToJSObject($arResult['CURRENCIES']) ?>;
+        var currencySelect = document.getElementById('currency-select');
+        var currencyResult = document.getElementById('currency-result');
 
-            currencyResult.innerHTML = '<p>Курс валюты <strong>' + currencyCode + '</strong>: ' + rate + '</p>'
+        function updateCurrencyRate() {
+            var selectedCurrency = currencySelect.value;
+            var rate = currencies[selectedCurrency]['AMOUNT'];
+            var currencyCode = selectedCurrency;
+            currencyResult.innerHTML = '<p>Курс валюты <strong>' + currencyCode + '</strong>: ' + rate + '</p>';
         }
-        updateCurrencyRate()
+
+        updateCurrencyRate();
         currencySelect.addEventListener('change', function() {
-            updateCurrencyRate()
-        })
-    })
+            updateCurrencyRate();
+        });
+    });
 </script>
